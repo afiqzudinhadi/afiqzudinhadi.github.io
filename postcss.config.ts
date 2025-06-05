460b092a6f5dd74602abfb49d95a092e99b1da98
afiqzudinhadi <afiqzudinhadi@gmail.com>
2025-06-06 00:18:06 +0800

    chore: fix shadcn/ui not applying styling
    https://stackoverflow.com/a/79431749
    
    rebase

---
+++ postcss.config.ts
@@ -0,0 +1,6 @@
+export default {
+	plugins: {
+		"@tailwindcss/postcss": require("@tailwindcss/postcss"),
+		autoprefixer: require("autoprefixer"),
+	},
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-	darkMode: ["class"],
-	content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "class",
+	content: [
+		"./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
+		"./app/**/*.{js,jsx,ts,tsx}",
+	],
 	theme: {
 		extend: {
 			borderRadius: {
