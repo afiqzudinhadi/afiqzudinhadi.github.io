7335ea25816bf399abf1f0065d3f990a6ff5606c
afiqzudinhadi <afiqzudinhadi@gmail.com>
2025-06-05 13:45:41 +0800

    init
    init
    
    init
    
    init

---
+++ app/root.tsx
@@ -0,0 +1,54 @@
+import {
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import styles from "./styles/global.css?url";
+
+export const links: LinksFunction = () => [
+	{
+		rel: "stylesheet",
+		href: styles,
+	},
+	{
+		rel: "preconnect",
+		href: "https://fonts.googleapis.com",
+	},
+	{
+		rel: "preconnect",
+		href: "https://fonts.gstatic.com",
+		crossOrigin: "anonymous",
+	},
+	{
+		rel: "stylesheet",
+		href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
+	},
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="data:image/x-icon;base64,AA" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<h1>Hello world!</h1>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
+
+export default function App() {
+	return <Outlet />;
+}
